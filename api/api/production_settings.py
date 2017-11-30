1e85a855794cdd322594cfb8ac1ecbedc059113c
Chathan Driehuys <chathan@driehuys.com>
2017-11-30 17:27:12 -0500

    Set up HTTPS.

--- api/api/production_settings.py
+++ api/api/production_settings.py
@@ -9,6 +9,13 @@ ALLOWED_HOSTS = [
 DEBUG = False
 
 
+# Security Settings for HTTPS
+
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+
+
 # Use the local settings created by Ansible if they exist
 
 try:
